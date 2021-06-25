9616bfe79838ead875f56b48eae09c109f1b0438
BarathKumar <barathganeshkumar2002@gmail.com>
2021-06-25 13:09:06 +0530

    Initial Commit

---
+++ SampleProject1/DetailedViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DetailedViewController.swift
+//  SampleProject1
+//
+//  Created by Barath Ganesh Kumar on 29/05/21.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage : String?
+    var total = 0
+    var curr = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Picture \(curr) of \(total)"
+        navigationItem.largeTitleDisplayMode = .never
+
+        if let imageToLoad = selectedImage{
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
